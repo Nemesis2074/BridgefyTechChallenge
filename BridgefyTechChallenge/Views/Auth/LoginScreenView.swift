d68afb5023c7ef97a1e456a7540a62ccd65fb5cf
Adair de Jesus Castillo Meza <nemesis2074@gmail.com>
2021-05-08 17:18:38 -0500

    - Se agregaron los Assets - Se creo el Splash Screen - Se creo el Login Screen

---
+++ BridgefyTechChallenge/Views/Auth/LoginScreenView.swift
@@ -0,0 +1,20 @@
+//
+//  LoginScreenView.swift
+//  BridgefyTechChallenge
+//
+//  Created by Adair de Jesús Castillo Meza on 08/05/21.
+//
+
+import UIKit
+
+class LoginScreenView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
